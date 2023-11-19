5f3e19e256a1d1bd3b91e6b96658c1574147a569
robhittme <robhittme@gmail.com>
2023-11-19 09:14:37 -0500

    adding more context

--- arrays_and_hashing/anagram/anagram.go
+++ arrays_and_hashing/anagram/anagram.go
@@ -1,34 +1,34 @@
 package main
 
-//	func isAnagram(s string, t string) bool {
-//		if len(s) != len(t) {
-//			return false
-//		}
-//		hash := make(map[rune]int)
-//		for _, c := range s {
-//			if hash[c] > 0 {
-//				hash[c]++
-//			} else {
-//				hash[c] = 1
-//			}
-//		}
-//		for _, b := range t {
-//			if hash[b] > 0 {
-//				hash[b]--
-//			} else {
-//				return false
-//			}
-//		}
-//		return true
-//	}
+func isAnagram1(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	hash := make(map[rune]int)
+	for _, c := range s {
+		if hash[c] > 0 {
+			hash[c]++
+		} else {
+			hash[c] = 1
+		}
+	}
+	for _, b := range t {
+		if hash[b] > 0 {
+			hash[b]--
+		} else {
+			return false
+		}
+	}
+	return true
+}
 func isAnagram(s string, t string) bool {
 	if len(s) != len(t) {
 		return false
 	}
 	var m [26]int
 	for i := 0; i < len(s); i++ {
-		m[s[i]]++
-		m[t[i]]--
+		m[s[i]-'a']++
+		m[t[i]-'a']--
 	}
 	for _, v := range m {
 		if v != 0 {
--- arrays_and_hashing/anagram/anagram_test.go
+++ arrays_and_hashing/anagram/anagram_test.go
@@ -13,6 +13,16 @@ var testCases = []struct {
 	{"top", "stop", false},
 }
 
+func TestIsAnagram1(t *testing.T) {
+	for _, tc := range testCases {
+		result := isAnagram1(tc.input1, tc.input2)
+		if result != tc.expected {
+			t.Errorf("isAnagram(%v, %v) = %v; expected %v", tc.input1, tc.input2, result, tc.expected)
+		}
+	}
+
+}
+
 func TestIsAnagram(t *testing.T) {
 	for _, tc := range testCases {
 		result := isAnagram(tc.input1, tc.input2)
